b96a5c534e670eaadfbbc51764587aff406f0561
Burov Ilya <burov.ilya@gmail.com>
2013-06-26 17:08:52 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Just a stub.
+Python script that parses postfix maillog and stores it into MySQL/MariaDB table.
+
+# Manual MySQL Table creation :
+
+CREATE TABLE IF NOT EXISTS `log` ( 
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `time` DATETIME NOT NULL,
+    `from` CHAR(60) NOT NULL,
+    `to` CHAR(60) NOT NULL,
+     PRIMARY KEY (`id`));
+
--- postfix2mysql.py
+++ postfix2mysql.py
@@ -1,18 +1,52 @@
 #!/usr/bin/python2
+# -*- coding: utf8 -*-
 
+import re;
+import MySQLdb;
+
+db_host = 'localhost';
+db_user = 'root';
+db_pass = 'toor';
+db_name = 'maillog'
+logfile = 'maillog';
+
+email = re.compile('<[a-zA-Z0-9@.-_]+>');
 badwords = ['warning:','disconnect'];
-records = {};
+
 class Record:
+    '''Structure for an event with uniq id'''
+    _from = '' ;
+    _to = '';
+    _time = '';
+
+def insertRecord(record):
     pass;
 
-maillog = open ('maillog','r');
+
+db = MySQLdb.connect(host=db_host, user=db_user, passwd=db_pass, db=db_name, charset='utf8');
+cursor = db.cursor();
+
+records = {};
+maillog = open(logfile,'r');
 for line in maillog:
-    (mailer, msgid) = line.split()[4:6];
+    (mailer, msgid, event) = line.split()[4:7];
     if mailer.startswith('postfix') and not (msgid in badwords):
         if not records.has_key(msgid):
                 records[msgid] = Record();
-            
+        if event.startswith('from'):
+                match = email.search(event);
+                if (match):
+                    records[msgid]._from = email.search(event).group(); 
+        if event.startswith('to'):
+                match = email.search(event);
+                if (match):
+                    records[msgid]._from = email.search(event).group(); 
+        if event.startswith('removed'):
+            pass;
+
 maillog.close();
 
 
-print records.keys(); 
+#for key in records.keys():
+#    print records[key]._from;
+
